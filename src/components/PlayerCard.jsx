8ee8ba8b4c39d7d1d41c986e015089ee5fed55e3
Muhammet Erol <muhammederol971@gmail.comgit config --global user.email muhammederol971@gmail.com>
2024-03-28 02:02:12 +0100

    finished card-click

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -1,27 +1,23 @@
 import { useState } from "react";
 
-
 const PlayerCard = ({ data }) => {
-
-
-  const [property, setProperty] = useState(data.statistics);
+  const [property, setProperty] = useState([]);
   const [toggle, setToggle] = useState(true);
 
   const handleEvent = () => {
-    setToggle(!toggle)
-    return setProperty(property => property.statistics )
-  }
-  
+    setProperty(data.statistics);
+    setToggle(!toggle);
+  };
   console.log(data);
   return (
-    <div className="card" onClick={handleEvent}>
-
-
-
-<p>{property}</p>
-
-      <img src={data.img} alt="" className="card-img" />
-      <p className="card-name">{data.name}</p>
+    <div
+      className={toggle ? "card" : "card-click-container"}
+      onClick={handleEvent}
+    >
+      <img src={data.img} alt="" className={toggle ? "card-img" : "statistics-img"} />
+      <div className={toggle ? "card-name" : "card-ul"}>
+        {toggle ? data.name : property.map((item) => <li>{item}</li>)}
+      </div>
     </div>
   );
 };
